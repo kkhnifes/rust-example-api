1c559efc9020b9ea5b0820309390a803aacdce2d
Kyrillus Khnifes <khnifes.kyrillus@gmail.com>
2024-03-31 03:04:35 +0200

    refactor: use modules
    Signed-off-by: Kyrillus Khnifes <khnifes.kyrillus@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,17 @@ use axum::{routing::get, Router};
 
 #[tokio::main]
 async fn main() {
-    let app: Router = Router::new().route("/", get(|| async { "Hello, World!" }));
+    let app: Router = Router::new().nest("/test", ping_pong());
 
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
 
     axum::serve(listener, app).await.unwrap();
+}
+
+fn ping_pong() -> Router {
+    async fn hello() -> &'static str {
+        "pong"
+    }
+    Router::new().route("/ping", get(hello))
 }
